0cfbf0b8d92a053c81d21d73b35d5401d987cdfa
Nerea <801950@unizar.es>
2023-09-28 12:09:32 -0700

    lab 2 exercise 1

--- catkin_ws/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/nerea/AR/catkin_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/nerea/AR/catkin_ws/build/catkin_tools_prebuild'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/nerea/AR/catkin_ws/devel/.private/catkin_tools_prebuild/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/nerea/AR/catkin_ws/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nerea/AR/catkin_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/nerea/AR/catkin_ws/build/catkin_tools_prebuild:/home/nerea/AR/catkin_ws/src/first_package:/home/nerea/AR/catkin_ws/src/p09_arob_lab2:/home/nerea/AR/catkin_ws/src/second_package:/home/nerea/AR/catkin_ws/src/teleop_twist_keyboard_cpp:/home/nerea/AR/catkin_ws/src/turtle_comunic:/opt/ros/noetic/share'
